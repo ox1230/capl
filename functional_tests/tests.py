da08da60947a5ebd42a879956be3dbd67f4e75af
ox1230 <ox11223300@hanmail.net>
2018-05-08 22:05:57 +0900

    Processing.add_history로 리팩토링전

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -15,6 +15,7 @@ class VisitorTest(LiveServerTestCase):
         #미리 설정되어 있는 데이터
         Category.objects.create(name = '군것질')
         Category.objects.create(name = '세끼')
+        Category.objects.create(name = '선물')
 
         
     
@@ -32,18 +33,23 @@ class VisitorTest(LiveServerTestCase):
 
         #메인 화면에는 현재 사용금액 정보가 있다. 현재 사용 금액은 0원이다.   -- 사용가능 금액은 300000이다.
        
-        rows =self.browser.find_elements_by_tag_name('tr')
-        self.assertIn('현재 사용금액 0원', [row.text for row in rows])
-        self.assertIn('남은 금액 300000원', [row.text for row in rows])
+        rows_text = self.find_rows_from_table_id("present_box")
+        self.assertIn('현재 사용금액 0원', rows_text)
+        self.assertIn('남은 금액 300000원', rows_text)
 
-        # (각 거래내역별 사용가능금액이 표시된다.)  -- 이부분 나중에 
+        # (각 category별 사용가능금액이 표시된다.)  -- 이부분 나중에 
+        
+        rows_text =  self.find_rows_from_table_id('detail_box')
 
+        self.assertIn('군것질 100000원', rows_text)
+        self.assertIn('세끼 100000원', rows_text)
+        self.assertIn('선물 100000원', rows_text)
 
         #메뉴에서 거래내역추가를 눌러 거래내역 추가로 이동한다.   -- 일단 하이퍼링크로 대체
         add_history_menu = self.browser.find_element_by_id("add_history_menu")
         add_history_menu.click()
 
-        time.sleep(2)
+        time.sleep(1)
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/add_history/')
 
@@ -54,13 +60,29 @@ class VisitorTest(LiveServerTestCase):
         self.browser.find_element_by_id("history_price_inputBox").send_keys('1500')
         self.browser.find_element_by_id("add_history_button").click()
         
-        # 다시 메인 화면으로 돌아가지고, 현재 사용금액등이 변경되어 있다.   (사용금액 1500원,    사용가능금액 298500)
+        
+        #이어서 세끼, 짜장상회, 4000원을 입력한다.
+        self.browser.find_element_by_id("history_category_inputBox").send_keys('세끼')
+        self.browser.find_element_by_id("history_name_inputBox").send_keys('짜장상회')
+        self.browser.find_element_by_id("history_price_inputBox").send_keys('4500')
+        self.browser.find_element_by_id("add_history_button").click()
+        
+        # 다시 메인 화면으로 돌아가지고, 현재 사용금액등이 변경되어 있다.   (사용금액 6000원,    사용가능금액 294000)
         # (계속 입력은 아직 추가 x)
        
         time.sleep(1)
-        present_box = self.browser.find_element_by_id("present_box")
-        rows = present_box.find_elements_by_tag_name('tr')
-        self.assertIn('현재 사용금액 1500원', [row.text for row in rows])
-        self.assertIn('남은 금액 298500원', [row.text for row in rows])
+        rows_text = self.find_rows_from_table_id("present_box")
+        self.assertIn('현재 사용금액 1500원', rows_text)
+        self.assertIn('남은 금액 294000원', rows_text )
+
+        rows_text =  self.find_rows_from_table_id('detail_box')
+        self.assertIn('군것질 98500원', rows_text)
+        self.assertIn('세끼 96000원', rows_text)
+        self.assertIn('선물 100000원', rows_text)
 
 
+    def find_rows_from_table_id(self, id):
+        """id에서 tr을 뽑아낸 후 각 row에서 text를 뽑아내 리스트로 만들어 리턴한다."""
+        table = self.browser.find_element_by_id(id)
+        rows = table.find_elements_by_tag_name('tr')
+        return [row.text for row in rows]
--- main/tests.py
+++ main/tests.py
@@ -111,3 +111,17 @@ class MainAndItemModelTest(TestCase):
         self.assertEqual(second_saved_history.category , cate_)
         self.assertEqual(second_saved_history.price , 2000)
 
+    
+    def test_processing_add_history(self):
+        Category.objects.create(name = '첫번째분류')
+        
+        cate = Category.objects.first()
+
+        self.assertEqual("첫번째분류", cate.name)
+        self.assertEqual(100000, cate.residual)
+
+        Processing.add_history("첫번째분류","거래내역",1000)
+        self.assertEqual("첫번째분류", cate.name)
+        self.assertEqual(100000, cate.residual)
+
+        
