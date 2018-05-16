4e385fa64b1ea850e5688ac553eb9b0f2cb4d680
ox1230 <ox11223300@hanmail.net>
2018-05-16 18:56:44 +0900

    돌아가기 버튼 추가

--- functional_tests/test_main_and_add_history_works.py
+++ functional_tests/test_main_and_add_history_works.py
@@ -14,7 +14,7 @@ class AlreadyVisitorTest(FuntionalTest):
         #edith가 해당 웹사이트 방문
         self.browser.get(self.live_server_url)
 
-        #메뉴에서 거래내역추가를 눌러 거래내역 추가로 이동한다.   -- 일단 하이퍼링크로 대체
+        #메뉴에서 거래내역추가를 눌러 거래내역 추가로 이동한다.
         add_history_menu = self.browser.find_element_by_id("add_history_menu")
         add_history_menu.click()
 
@@ -50,4 +50,28 @@ class AlreadyVisitorTest(FuntionalTest):
         self.assertIn('세끼 95500원', rows_text)
         self.assertIn('기타 100000원', rows_text)
 
-   
+    def test_can_go_to_main_by_button(self):
+        #에디스는 사이트를 방문해 거래내역추가로 이동한다
+  
+        self.browser.get(self.live_server_url)
+
+        add_history_menu = self.browser.find_element_by_id("add_history_menu")
+        add_history_menu.click()
+
+        #에디스는 각 내역을 입력했지만, 취소하고 메인으로 돌아가고 싶다
+        self.browser.find_element_by_id("history_category_inputBox").send_keys('군것질')
+        self.browser.find_element_by_id("history_name_inputBox").send_keys('딸기바나나')
+        self.browser.find_element_by_id("history_price_inputBox").send_keys('1500')
+
+        self.browser.find_element_by_id("go_to_main").click()
+
+        # 메인에는 아무것도 바뀌어있지 않다.
+        time.sleep(1)
+        rows_text = self.find_rows_from_table_id("present_box")
+        self.assertIn('현재 사용금액 0원', rows_text)
+        self.assertIn('남은 금액 300000원', rows_text )
+
+        rows_text =  self.find_rows_from_table_id('detail_box')
+        self.assertIn('군것질 100000원', rows_text)
+        self.assertIn('세끼 100000원', rows_text)
+        self.assertIn('기타 100000원', rows_text)
